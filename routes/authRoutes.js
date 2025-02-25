7697671d606c0120f6e73ba1c89c87f287f65d15
Chirag Jethva <chiragjethva23@gmail.com>
2025-02-25 13:53:07 +0530

    add

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { signup } = require("../controllers/authController");
-
-const router = express.Router();
-
-// Signup Route
-router.post("/signup", signup);
-
-module.exports = router;
